cac593d49e09605a880824554255586787d46149
Brett-Robinson <brett.robinson93@gmail.com>
2015-04-10 16:01:08 -0600

    added a second label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
- 
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +28,15 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        enterNameTextField.text = ""
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
